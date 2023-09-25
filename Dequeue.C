88ed712a61e9403f482c7a9640b824cddf080f59
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-09-25 22:34:42 +0530

    Add files via upload

---
+++ Dequeue.C
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct queue{
+    int size;
+    int f;
+    int r;
+    int* arr;
+};
+
+int isEmpty(struct queue* q){
+    if(q->f == q->r){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int isFull(struct queue* q){ // Checking for full
+    if(q->r == q->size - 1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void enqueue(struct queue* q, int val){
+    if(isFull(q)){
+        printf("Queue Overflow\n");
+    }else{
+        q->r++;
+        q->arr[q->r] = val;
+    }
+}
+
+int dequeue(struct queue* q){ // Dequeue operation
+    int a = -1;
+    if(isEmpty(q)){
+        printf("Queue underflow\n");
+    }else{
+        q->f++;
+        a = q->arr[q->f];
+    }
+    return a;
+}
+
+int main(){
+    struct queue q;
+    q.size = 10;
+    q.f = -1;
+    q.r = -1;
+    q.arr = (int*)malloc(q.size * sizeof(int)); // Created array
+
+    if(isEmpty(&q)){
+        printf("Queue is empty\n");
+    }
+    // Enqueue few elements
+
+    enqueue(&q,12);
+    enqueue(&q,15);
+
+    printf("Dequeuing elements %d\n",dequeue(&q));
+    printf("Dequeuing elements %d\n",dequeue(&q));
+
+    if(isEmpty(&q)){
+        printf("Queue is empty\n");
+    }
+
+    if(isFull(&q)){
+        printf("Queue is full\n");
+    }
+    
+    return 0;
+
+}
