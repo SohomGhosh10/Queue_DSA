d17750de3060f89df7e7d2e76da04d8f221986a0
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-10-01 18:55:34 +0530

    Update CircularQueue.C

--- CircularQueue.C
+++ CircularQueue.C
@@ -18,7 +18,7 @@ int isEmpty(struct queue* q){
 }
 
 int isFull(struct queue* q){
-    if((q->r + 1) % q->size == q->f){
+    if((q->r + 1) % q->size == q->f){ // (q->r == q->size - 1) in linear queue
         return 1;
     }else{
         return 0;
@@ -29,7 +29,7 @@ void enqueue(struct queue* q, int val){
     if(isFull(q)){
         printf("Queue overflow\n");
     }else{
-        q->r = (q->r + 1) % q->size;
+        q->r = (q->r + 1) % q->size; // (q->r++) in linear queue
         q->arr[q->r] = val;
         printf("Enqueued value: %d\n",val);
     }
@@ -40,7 +40,7 @@ int dequeue(struct queue* q){
     if(isEmpty(q)){
         printf("Queue underflow\n");
     }else{
-        q->f = (q->f + 1) % q->size;
+        q->f = (q->f + 1) % q->size; // (q->f++) in linear queue
         a = q->arr[q->f];
     }
     return a;
@@ -59,9 +59,13 @@ int main(){
     enqueue(&q , 15);
     enqueue(&q , 1);
 
+    //Dequeue elements
+
     printf("Dequeuing elements %d\n",dequeue(&q));
     printf("Dequeuing elements %d\n",dequeue(&q));
     printf("Dequeuing elements %d\n",dequeue(&q));
+
+    // Enqueue elements again
 
     enqueue(&q , 45);
     enqueue(&q , 45);
