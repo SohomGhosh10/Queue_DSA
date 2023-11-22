fbb2405c5abeb70d7d51b15b54b58b0f9fc985fa
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-11-22 16:58:59 +0530

    Update Reverse_Queue.C

--- Reverse_Queue.C
+++ Reverse_Queue.C
@@ -1,71 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MAX_SIZE 100
 
-// Structure to represent a queue
+// Structure for the queue
 struct Queue {
-    int front, rear, size;
-    unsigned capacity;
-    int* array;
+    int arr[MAX_SIZE];
+    int front, rear;
 };
 
-// Function to create a new queue
-struct Queue* createQueue(unsigned capacity) {
-    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
-    if (!queue)
-        return NULL;
-
-    queue->capacity = capacity;
-    queue->front = queue->size = 0;
-    queue->rear = capacity - 1;
-    queue->array = (int*)malloc(queue->capacity * sizeof(int));
-    if (!queue->array)
-        return NULL;
-
-    return queue;
+// Function to initialize a new queue
+void initQueue(struct Queue* queue) {
+    queue->front = -1;
+    queue->rear = -1;
 }
 
 // Function to check if the queue is empty
 int isEmpty(struct Queue* queue) {
-    return (queue->size == 0);
+    return (queue->front == -1 && queue->rear == -1);
 }
 
 // Function to check if the queue is full
 int isFull(struct Queue* queue) {
-    return (queue->size == queue->capacity);
+    return (queue->rear + 1) % MAX_SIZE == queue->front;
 }
 
-// Function to enqueue an element
-void enqueue(struct Queue* queue, int item) {
-    if (isFull(queue))
+// Function to enqueue a new element to the queue
+void enqueue(struct Queue* queue, int data) {
+    if (isFull(queue)) {
+        printf("Queue is full. Cannot enqueue %d\n", data);
         return;
+    }
 
-    queue->rear = (queue->rear + 1) % queue->capacity;
-    queue->array[queue->rear] = item;
-    queue->size++;
+    if (isEmpty(queue)) {
+        queue->front = queue->rear = 0;
+    } else {
+        queue->rear = (queue->rear + 1) % MAX_SIZE;
+    }
+
+    queue->arr[queue->rear] = data;
 }
 
-// Function to dequeue an element
+// Function to dequeue an element from the queue
 int dequeue(struct Queue* queue) {
-    if (isEmpty(queue))
+    if (isEmpty(queue)) {
+        printf("Queue is empty. Cannot dequeue\n");
         return -1;
+    }
 
-    int item = queue->array[queue->front];
-    queue->front = (queue->front + 1) % queue->capacity;
-    queue->size--;
-    return item;
+    int data = queue->arr[queue->front];
+
+    if (queue->front == queue->rear) {
+        queue->front = queue->rear = -1;
+    } else {
+        queue->front = (queue->front + 1) % MAX_SIZE;
+    }
+
+    return data;
 }
 
-// Function to reverse a queue using a stack
+// Function to reverse a queue
 void reverseQueue(struct Queue* queue) {
-    if (isEmpty(queue))
-        return;
-
-    int* stack = (int*)malloc(queue->size * sizeof(int));
+    int stack[MAX_SIZE];
     int top = -1;
 
-    // Push all elements from the queue to the stack
+    // Push all elements of the queue onto the stack
     while (!isEmpty(queue)) {
         stack[++top] = dequeue(queue);
     }
@@ -74,35 +72,43 @@ void reverseQueue(struct Queue* queue) {
     while (top >= 0) {
         enqueue(queue, stack[top--]);
     }
-
-    free(stack);
 }
 
-// Function to print the elements in the queue
+// Function to print the elements of a queue
 void printQueue(struct Queue* queue) {
-    int i;
-    for (i = 0; i < queue->size; i++) {
-        printf("%d ", queue->array[(queue->front + i) % queue->capacity]);
+    if (isEmpty(queue)) {
+        printf("Queue is empty.\n");
+        return;
     }
+
+    int i = queue->front;
+    do {
+        printf("%d ", queue->arr[i]);
+        i = (i + 1) % MAX_SIZE;
+    } while (i != (queue->rear + 1) % MAX_SIZE);
+
     printf("\n");
 }
 
+// Main function
 int main() {
-    struct Queue* queue = createQueue(MAX_SIZE);
-    int i;
+    struct Queue queue;
+    initQueue(&queue);
 
     // Enqueue elements to the queue
-    for (i = 1; i <= 5; i++) {
-        enqueue(queue, i);
-    }
+    enqueue(&queue, 1);
+    enqueue(&queue, 2);
+    enqueue(&queue, 3);
+    enqueue(&queue, 4);
 
     printf("Original Queue: ");
-    printQueue(queue);
+    printQueue(&queue);
 
-    reverseQueue(queue);
+    // Reverse the queue
+    reverseQueue(&queue);
 
     printf("Reversed Queue: ");
-    printQueue(queue);
+    printQueue(&queue);
 
     return 0;
 }
